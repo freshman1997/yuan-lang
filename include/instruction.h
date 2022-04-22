3ce4ee86d51264e043246ec5049482b17446d2df
yuan <1905269298@qq.com>
2022-04-22 14:31:51 +0800

    20220 4-22

---
+++ include/instruction.h
@@ -0,0 +1,24 @@
+#ifndef __INSTRUCTION_H__
+#define __INSTRUCTION_H__
+
+
+
+struct InstructionParam
+{
+        
+};
+
+enum class OpCode
+{
+
+};
+
+struct Instruction
+{
+    OpCode op;
+    InstructionParam *param;
+
+
+};
+
+#endif
---
+++ include/visitor.h
@@ -0,0 +1,6 @@
+#ifndef __VISITOR_H__
+#define __VISITOR_H__
+
+
+
+#endif
