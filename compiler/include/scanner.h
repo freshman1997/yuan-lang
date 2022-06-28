c245941cc59f117afa85ef70756bc2ca1d8a6c55
yuan <1905269298@qq.com>
2022-06-28 09:54:52 +0800

    词法分析

--- compiler/include/scanner.h
+++ compiler/include/scanner.h
@@ -1,13 +1,16 @@
-#ifndef __SCANNER_H__
+﻿#ifndef __SCANNER_H__
 #define __SCANNER_H__
 
 #include <string>
 #include <vector>
+#include <unordered_map>
+
 using std::string;
 using std::vector;
+using std::unordered_map;
 
 
-class Position
+struct Position
 {
     int begin = 0;
     int end = 0;
@@ -17,18 +20,163 @@ class Position
 
 enum class TokenType
 {
-    number = 0,             // 数字
-    str,                    // 字符串
-    iden,                   // 标识符，定义的变量等
-    keyword,                // 关键字
-    symbol,                 // 符号
+    _integer_literal = 0,           // 整形字面量
+    _double_literal,                // 浮点字面量
+    _string_literal,                // 字符串字面量
+    _identifier,                    // 标识符，定义的变量等
+    _keyword,                       // 关键字
+    _operator,                      // 操作符 
+    _seperator,                     // 符号 [] () {} ; :
+    _eof,
 };
 
-class Token
+enum class KeyWord
+{
+    _int8_ = 0,
+    _uint8_,
+    _int16_,
+    _uint16_,
+    _int32_, 
+    _uint32_,
+    _int64_,
+    _uint64_,
+    _float32_,
+    _float64_,
+    _void_,
+    _let_,
+    _return_,
+    _class_,
+    _extends_,
+    _implements_,
+    _interface_,
+    _static_,
+    _goto_,
+    _for_,
+    _switch_,
+    _case_,
+    _default_,
+    _do_,
+    _while_,
+    _enum_,
+    _if_,
+    _else_,
+    _break_,
+    _continue_,
+    _true_,
+    _false_,
+    _null_,
+    _new_,
+    _try_,
+    _catch_,
+    _throw_,
+    _finally_,
+    _export_,
+    _import_,
+    _from_,
+    _public_,
+    _protected_,
+    _private_,
+    _this_,
+    _super_,
+    _instanceof_,
+};
+
+enum class Seperator
+{
+    OpenBracket = 0,                //[
+    CloseBracket,                   //]
+    OpenParen,                      //(
+    CloseParen,                     //)
+    OpenBrace,                      //{
+    CloseBrace,                     //}
+    Colon,                          //:
+    SemiColon,                      //;
+};
+
+enum class Op
+{
+    QuestionMark = 100,             //?   让几个类型的code取值不重叠
+    Ellipsis,                       //...
+    Dot,                            //.
+    Comma,                          //,
+    At,                             //@
+    
+    RightShiftArithmetic,           //>>
+    LeftShiftArithmetic,            //<<
+    RightShiftLogical,              //>>>
+    IdentityEquals,                 //===
+    IdentityNotEquals,              //!==
+
+    BitNot,                         //~
+    BitAnd,                         //&
+    BitXOr,                         //^
+    BitOr,                          //|
+
+    Not,                            //!   
+    And,                            //&&
+    Or,                             //||
+
+    Assign,                         //=
+    MultiplyAssign,                 //*=
+    DivideAssign,                   ///=
+    ModulusAssign,                  //%=
+    PlusAssign,                     //+=
+    MinusAssign,                    //-=
+    LeftShiftArithmeticAssign,      //<<=
+    RightShiftArithmeticAssign,     //>>=
+    RightShiftLogicalAssign,        //>>>=
+    BitAndAssign,                   //&=
+    BitXorAssign,                   //^=
+    BitOrAssign,                    //|=
+    
+    ARROW,                          //=>
+
+    Inc,                            //++
+    Dec,                            //--
+
+    Plus,                           //+
+    Minus,                          //-
+    Multiply,                       //*
+    Divide,                         ///
+    Modulus,                        //%
+    
+    EQ,                             //==
+    NE,                             //!=
+    G,                              //>
+    GE,                             //>=
+    L,                              //<
+    LE,                             //<=
+};
+
+// op utils
+bool isAssignOp(Op op);
+bool isRelationOp(Op op);
+bool isArithmeticOp(Op op);
+bool isLogicalOp(Op op);
+
+enum class CodeType
+{
+    _op,
+    _sep,
+    _kw,
+    _null,
+};
+
+typedef union 
+{
+    CodeType type;
+    Op op;
+    Seperator sep;
+    KeyWord kw;
+    char null;
+} Code;
+
+struct Token
 {
     TokenType type;
     string text;
-    
+    Position pos;
+    Code code;
 };
 
 class CharStream
@@ -38,8 +186,7 @@ public:
     char next();
     bool eof();
     Position get_posistion();
-
-private:
+    CharStream(const char *_data);
     const char *data = NULL;
     int line;
     int col;
@@ -53,7 +200,19 @@ public:
     const Token & peek();
     const Token & peek(int i);
 private:
-    void getAToken();    
+    Token getAToken();
+    void skipWhiteSpace();
+    void skipSingleLineComment();
+    void skipMultiLineComment();
+
+    bool isDigit(char ch);
+    bool isLetter(char ch);
+    bool isLetterDigitOrUnderScore(char ch);
+    
+
+    Token parseStringLiteral();
+    Token parseNumberLiteral();
+    Token parseIdentifer();
 
 private:
     CharStream *stream = NULL;
@@ -61,6 +220,20 @@ private:
 
     // 用于支持预读多个 Token 的
     vector<Token> peekTokens;
+
+    unordered_map<string, KeyWord> keywords = {
+        {"int8", KeyWord::_int8_},
+        {"uint8", KeyWord::_uint8_},
+        {"int16", KeyWord::_int16_},
+        {"uint16", KeyWord::_uint16_},
+        {"int32", KeyWord::_int32_},
+        {"uint32", KeyWord::_uint32_},
+        {"int64", KeyWord::_int64_},
+        {"uint64", KeyWord::_uint64_},
+        {"float32", KeyWord::_float32_},
+        {"float64", KeyWord::_float64_},
+        {"return", KeyWord::_return_},
+    };
 };
 
 #endif
--- compiler/src/scanner.cpp
+++ compiler/src/scanner.cpp
@@ -1,3 +1,621 @@
+﻿#include <cassert>
 #include "scanner.h"
 
+CharStream::CharStream(const char *_data) : data(_data){}
+
+char CharStream::next()
+{
+    char ch = this->data[pos++];
+    if(ch == '\n') {
+        this->line ++;
+        this->col = 1;
+    }else {
+        this->col ++;
+    }
+    return ch;
+}
+
+char CharStream::peek()
+{
+    return this->data[pos];
+}
+
+bool CharStream::eof()
+{
+    return this->peek() == '\0';
+}
+
+Position CharStream::get_posistion()
+{
+    return {this->pos+1, this->pos+1, this->line, this->col};
+}
+
+Token Scanner::next()
+{
+    if (peekTokens.empty()) {
+        const Token &t = getAToken();
+        peekTokens.push_back(getAToken());
+        return t;
+    }
+    else {
+        const Token &t = peekTokens.front();
+        peekTokens.pop_back();
+        lastPos = t.pos;
+        return t;
+    }
+}
+
+const Token & Scanner::peek()
+{
+    return this->peekTokens.front();
+}
+
+const Token & Scanner::peek(int i)
+{
+    if (peekTokens.size() < i) {
+        int c = int(peekTokens.size()) - 1;
+        while (c <= i) {
+            peekTokens.push_back(getAToken());
+            ++c;
+        }
+    }
+    return this->peekTokens[i];
+}
+
+/*
+ * 跳过空白字符
+ */
+void Scanner::skipWhiteSpace()
+{
+    char ch = stream->peek();
+    while (ch == ' ' || ch == '\t' || ch == '\n') {
+        ch = stream->next();
+    }
+}
+
+/*
+ * 跳过单行注释
+ */
+void Scanner::skipSingleLineComment()
+{
+    stream->next();
+    //往后一直找到回车或者eof
+    while(this->stream->peek() !='\n' && !this->stream->eof()){
+        this->stream->next();
+    }
+}
+
+/*
+ * 跳过多行注释
+ */
+void Scanner::skipMultiLineComment()
+{
+    //跳过*，/之前已经跳过去了。
+    this->stream->next();
+
+    if (!this->stream->eof()){
+        char ch1 = this->stream->next();
+        //往后一直找到回车或者eof
+        while(!this->stream->eof()){
+            char ch2 = this->stream->next();
+            if (ch1 == '*' && ch2 == '/'){
+                return;
+            }
+            ch1 = ch2;
+        }
+    }
+
+    // TODO error report
+
+}
+
+bool Scanner::isDigit(char ch)
+{
+    return ch >= '0' && ch <= '9';
+}
+
+bool Scanner::isLetter(char ch)
+{
+    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+}
+
+bool Scanner::isLetterDigitOrUnderScore(char ch)
+{
+    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') || ch == '_';
+}
+
+Token Scanner::parseStringLiteral()
+{
+    return {};
+}
+
+Token Scanner::parseNumberLiteral()
+{
+    return {};
+}  
+
+Token Scanner::parseIdentifer()
+{
+    Position pos = stream->get_posistion();
+    Token token = {TokenType::_identifier, "", pos, {}};
+    
+    //第一个字符不用判断，因为在调用者那里已经判断过了
+    token.text.push_back(stream->next());
+    
+    //读入后序字符
+    while(!stream->eof() &&
+            isLetterDigitOrUnderScore(stream->peek())){
+        token.text.push_back(stream->next());
+    }
+
+    pos.end = stream->pos + 1;
+
+    //识别出关键字（从字典里查，速度会比较快）
+    if (this->keywords.count(token.text)){
+        token.type = TokenType::_keyword;
+        token.code.type = CodeType::_kw;
+        token.code.kw = this->keywords[token.text];
+    }
+
+    return token;
+}
+
+Token Scanner::getAToken()
+{
+    skipWhiteSpace();
+    Position pos = stream->get_posistion();
+    if (stream->eof()) {
+        return {TokenType::_eof, "", {}};
+    }
+
+    char ch = stream->peek();
+    Code code;
+
+    if (this->isLetter(ch) || ch == '_'){
+        return this->parseIdentifer();
+    }
+    else if (ch == '('){
+        stream->next();
+        code.type = CodeType::_sep;
+        code.sep = Seperator::OpenParen;
+        return {TokenType::_seperator, "(", pos, code};
+    }
+    else if (ch == ')'){
+        stream->next();
+        code.type = CodeType::_sep;
+        code.sep = Seperator::CloseParen;
+        return {TokenType::_seperator, ")", pos, code};
+    }
+    else if (ch == '{'){
+        stream->next();
+        code.type = CodeType::_sep;
+        code.sep = Seperator::OpenBrace;
+        return {TokenType::_seperator, "{", pos, code};
+    }
+    else if (ch == '}'){
+        stream->next();
+        code.type = CodeType::_sep;
+        code.sep = Seperator::CloseBrace;
+        return {TokenType::_seperator, "}", pos, code};
+    }
+    else if (ch == '['){
+        stream->next();
+        code.type = CodeType::_sep;
+        code.sep = Seperator::OpenBracket;
+        return {TokenType::_seperator, "[", pos, code};
+    }
+    else if (ch == ']'){
+        stream->next();
+        code.type = CodeType::_sep;
+        code.sep = Seperator::CloseBracket;
+        return {TokenType::_seperator, "]", pos, code};
+    }
+    else if (ch == ':'){
+        stream->next();
+        code.type = CodeType::_sep;
+        code.sep = Seperator::Colon;
+        return {TokenType::_seperator, ":", pos, code};
+    }
+    else if (ch == ';'){
+        stream->next();
+        code.type = CodeType::_sep;
+        code.sep = Seperator::SemiColon;
+        return {TokenType::_seperator, ";", pos, code};
+    }
+    else if (ch == ','){
+        stream->next();
+        code.type = CodeType::_op;
+        code.op = Op::Comma;
+        return {TokenType::_seperator, ",", pos, code};
+    }
+    else if (ch == '?'){
+        stream->next();
+        code.type = CodeType::_op;
+        code.op = Op::QuestionMark;
+        return {TokenType::_seperator, "?", pos, code};
+    }
+    //解析数字字面量，语法是：
+    // DecimalLiteral: IntegerLiteral '.' [0-9]* 
+    //   | '.' [0-9]+
+    //   | IntegerLiteral 
+    //   ;
+    // IntegerLiteral: '0' | [1-9] [0-9]* ;
+    else if (isDigit(ch)){
+        stream->next();
+        char ch1 = stream->peek();
+        string literal;
+        if(ch == '0'){//暂不支持八进制、二进制、十六进制
+            if (!(ch1>='1' && ch1<='9')){
+                literal.push_back('0');
+            }
+            else {
+                //console.log("0 cannot be followed by other digit now, at line: " + stream->line + " col: " + stream->col);
+                //暂时先跳过去
+                stream->next();
+                return getAToken();
+            }
+        }
+        else if(ch >= '1' && ch <= '9'){
+            literal.push_back(ch);
+            while(isDigit(ch1)){
+                ch = stream->next();
+                literal.push_back(ch);
+                ch1 = stream->peek();
+            }
+        }
+        //加上小数点.
+        if (ch1 == '.'){
+            //小数字面量
+            literal.push_back('.');
+            stream->next();
+            ch1 = stream->peek();
+            while(isDigit(ch1)){
+                ch = stream->next();
+                literal.push_back(ch);
+                ch1 = stream->peek();
+            }
+            pos.end = stream->pos + 1;
+            return {TokenType::_double_literal, literal, pos, {}};
+        }
+        else{
+            //返回一个整型字面量
+            return {TokenType::_integer_literal, literal, pos, {}};
+        }
+    }
+    else if (ch == '.'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (isDigit(ch1)){
+            //小数字面量
+            string literal = ".";
+            while(isDigit(ch1)){
+                ch = stream->next();
+                literal.push_back(ch);
+                ch1 = stream->peek();
+            }
+            pos.end = stream->pos+1;
+            return {TokenType::_double_literal, literal, pos, {}};
+        }
+        //...省略号
+        else if (ch1 == '.'){
+            stream->next();
+            //第三个.
+            ch1 = stream->peek();
+            if (ch1 == '.'){
+                pos.end = stream->pos+1;
+                code.type = CodeType::_op;
+                code.op = Op::Ellipsis;
+                return {TokenType::_seperator, "...", pos, code};
+            }
+            else{
+                //console.log('Unrecognized pattern : .., missed a . ?');
+                return getAToken();
+            }
+        }
+        //.号分隔符
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::Dot;
+            return {TokenType::_operator, ".", pos, code};
+            //return new Token(TokenType::_operator, '.', pos, Op.Dot);
+        }
+    }
+    else if (ch == '/'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '*'){
+            skipMultiLineComment();
+            return getAToken();
+        }
+        else if (ch1 == '/'){
+            return getAToken();
+            skipSingleLineComment();
+        }
+        else if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::DivideAssign;
+            return {TokenType::_operator, "/=", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::Divide;
+            return {TokenType::_operator, "/", pos, code};
+        }
+    }
+    else if (ch == '+'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '+'){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::Inc;
+            return {TokenType::_operator, "++", pos, code};
+        }else if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::PlusAssign;
+            return {TokenType::_operator, "+=", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::Plus;
+            return {TokenType::_operator, "+", pos, code};
+        }
+    }
+    else if (ch == '-'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '-'){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::Dec;
+            return {TokenType::_operator, "--", pos, code};
+        }else if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::MinusAssign;
+            return {TokenType::_operator, "-=", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::Minus;
+            return {TokenType::_operator, "-", pos, code};
+        }
+    }
+    else if (ch == '*'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::MultiplyAssign;
+            return {TokenType::_operator, "*=", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::Multiply;
+            return {TokenType::_operator, "*", pos, code};
+        }
+    }
+    else if (ch == '%'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::ModulusAssign;
+            return {TokenType::_operator, "%=", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::Modulus;
+            return {TokenType::_operator, "%", pos, code};
+        }
+    }
+    else if (ch == '>'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::GE;
+            return {TokenType::_operator, ">=", pos, code};
+        }
+        else if (ch1 == '>'){
+            stream->next();
+            char ch1 = stream->peek();
+            if (ch1 == '>'){
+                stream->next();
+                ch1 = stream->peek();
+                if (ch1 == '='){
+                    stream->next();
+                    pos.end = stream->pos+1;
+                    code.type = CodeType::_op;
+                    code.op = Op::RightShiftLogicalAssign;
+                    return {TokenType::_operator, ">>>=", pos, code};
+                }
+                else{
+                    pos.end = stream->pos+1;
+                    code.type = CodeType::_op;
+                    code.op = Op::RightShiftLogical;
+                    return {TokenType::_operator, ">>>", pos, code};
+                }
+            }
+            else if (ch1 == '='){
+                stream->next();
+                pos.end = stream->pos+1;
+                code.type = CodeType::_op;
+                code.op = Op::LeftShiftArithmeticAssign;
+                return {TokenType::_operator, ">>=", pos, code};
+            }
+            else{
+                pos.end = stream->pos+1;
+                code.type = CodeType::_op;
+                code.op = Op::RightShiftArithmetic;
+                return {TokenType::_operator, ">>", pos, code};
+            }
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::G;
+            return {TokenType::_operator, ">", pos, code};
+        }
+    }
+    else if (ch == '<'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::LE;
+            return {TokenType::_operator, "<=", pos, code};
+        }
+        else if (ch1 == '<'){
+            stream->next();
+            ch1 = stream->peek();
+            if (ch1 == '='){
+                stream->next();
+                pos.end = stream->pos + 1;
+                code.type = CodeType::_op;
+                code.op = Op::LeftShiftArithmeticAssign;
+                return {TokenType::_operator, "<<=", pos, code};
+            }
+            else{
+                pos.end = stream->pos+1;
+                code.type = CodeType::_op;
+                code.op = Op::LeftShiftArithmetic;
+                return {TokenType::_operator, "<<", pos, code};
+            }
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::L;
+            return {TokenType::_operator, "<", pos, code};
+        }
+    }
+    else if (ch == '='){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::EQ;
+            return {TokenType::_operator, "==", pos, code};
+        }
+        //箭头=>
+        else if (ch1 == '>'){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::ARROW;
+            return {TokenType::_operator, "=>", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::Assign;
+            return {TokenType::_operator, "=", pos, code};
+        }
+    }
+    else if (ch == '!'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::NE;
+            return {TokenType::_operator, "!=", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::Not;
+            return {TokenType::_operator, "!", pos, code};
+        }
+    }
+    else if (ch == '|'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '|'){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::Or;
+            return {TokenType::_operator, "||", pos, code};
+        }
+        else if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::BitOrAssign;
+            return {TokenType::_operator, "|=", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::BitOr;
+            return {TokenType::_operator, "|", pos, code};
+        }
+    }
+    else if (ch == '&'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '&'){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::And;
+            return {TokenType::_operator, "&&", pos, code};
+        }
+        else if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::BitAndAssign;
+            return {TokenType::_operator, "&=", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::BitAnd;
+            return {TokenType::_operator, "&", pos, code};
+        }
+    }
+    else if (ch == '^'){
+        stream->next();
+        char ch1 = stream->peek();
+        if (ch1 == '='){
+            stream->next();
+            pos.end = stream->pos+1;
+            code.type = CodeType::_op;
+            code.op = Op::BitXorAssign;
+            return {TokenType::_operator, "^=", pos, code};
+        }
+        else{
+            code.type = CodeType::_op;
+            code.op = Op::BitXOr;
+            return {TokenType::_operator, "^", pos, code};
+        }
+    }
+    else if (ch == '~'){
+        stream->next();
+        code.type = CodeType::_op;
+        code.op = Op::BitNot;
+        return {TokenType::_operator, "~", pos, code};
+    }
+    else{ 
+        //暂时去掉不能识别的字符
+        //console.log("Unrecognized pattern meeting ': " +ch+"', at ln:" + stream->line + " col: " + stream->col);
+        stream->next();
+        return getAToken();
+    }
+}
 
